8a89b1b030cd1cbba947458331a9a9a64dc988dd
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-07-28 20:25:14 -0400

    Start set up React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Users}></Route>
+        <Route exact path='/places/new' component={NewPlace}></Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
