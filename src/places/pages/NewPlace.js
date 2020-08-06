62186a07419ebf33d1d671181c0d31b07527c2f0
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-08-06 12:53:49 -0400

    Add state in the Input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -6,7 +6,12 @@ import Input from '../../shared/components/FormElements/Input';
 const NewPlace = () => {
   return (
     <form className='place-form'>
-      <Input element='input' type='text' label='Title'></Input>
+      <Input
+        element='input'
+        type='text'
+        label='Title'
+        errorText='Please enter a valid title'
+      ></Input>
     </form>
   );
 };
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,18 +1,60 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './Input.css';
 
-const Input = ({ label, id, element, type, placeholder, rows }) => {
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.payload,
+        isValid: true,
+      };
+    default:
+      return state;
+  }
+};
+
+const Input = ({ label, id, element, type, placeholder, rows, errorText }) => {
+  // The second argument is the initial state
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: '',
+    isValid: false,
+  });
+
+  const changeHandler = (e) => {
+    dispatch({
+      type: 'CHANGE',
+      payload: e.target.value,
+    });
+  };
+
   const el =
     element === 'input' ? (
-      <input id={id} type={type} placeholder={placeholder} />
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        onChange={changeHandler}
+        value={inputState.value}
+      />
     ) : (
-      <textarea id={id} rows={rows || 3}></textarea>
+      <textarea
+        id={id}
+        rows={rows || 3}
+        onChange={changeHandler}
+        value={inputState.value}
+      ></textarea>
     );
   return (
-    <div className={`form-control`}>
+    <div
+      className={`form-control ${
+        !inputState.isValid && 'form-control--invalid'
+      }`}
+    >
       <label htmlFor={id}>{label}</label>
       {el}
+      {!inputState.isValid && <p>{errorText}</p>}
     </div>
   );
 };
