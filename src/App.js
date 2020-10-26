439c3fce7de5558204e45261e9bb27165fa9d44b
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-10-26 18:30:01 -0400

    Add AuthContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
@@ -8,21 +8,40 @@ import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <Router>
-      <MainNav></MainNav>
-      <main>
-        <Switch>
-          <Route exact path='/' component={Users}></Route>
-          <Route exact path='/places/new' component={NewPlace}></Route>
-          <Route exact path='/:userId/places' component={UserPlaces}></Route>
-          <Route exact path='/places/:placeId' component={UpdatePlace}></Route>
-          <Route exact path='/auth' component={Auth}></Route>
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNav></MainNav>
+        <main>
+          <Switch>
+            <Route exact path='/' component={Users}></Route>
+            <Route exact path='/places/new' component={NewPlace}></Route>
+            <Route exact path='/:userId/places' component={UserPlaces}></Route>
+            <Route
+              exact
+              path='/places/:placeId'
+              component={UpdatePlace}
+            ></Route>
+            <Route exact path='/auth' component={Auth}></Route>
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
