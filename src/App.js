1d06312071aa4aa3dafb739e8b2b2aabde2f0727
Charles-Hebert Dalzon <charlby5@gmail.com>
2020-11-01 16:05:07 -0500

    Create auto-login and auto-logout functionnality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -15,19 +15,56 @@ import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
 import { AuthContext } from './shared/context/auth-context';
 
+let logoutTimer;
+
 const App = () => {
   const [token, setToken] = useState();
   const [userId, setUserId] = useState(null);
+  const [tokenExpiration, setTokenExpiration] = useState();
 
-  const login = useCallback((uId, token) => {
+  const login = useCallback((uId, token, expirationDate) => {
     setToken(token);
     setUserId(uId);
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60); //1000 * 60 * 60 = 1h
+    setTokenExpiration(tokenExpirationDate);
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userId: uId,
+        token: token,
+        expiration: tokenExpirationDate.toISOString(),
+      })
+    );
   }, []);
 
   const logout = useCallback(() => {
     setToken();
     setUserId(null);
+    setTokenExpiration(null);
+    localStorage.removeItem('userData');
   }, []);
+
+  // Create a time which logout the user when the token expires
+  useEffect(() => {
+    if (token && tokenExpiration) {
+      const remainingTime = tokenExpiration.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpiration]);
+
+  useEffect(() => {
+    const storeData = JSON.parse(localStorage.getItem('userData'));
+    if (
+      storeData &&
+      storeData.token &&
+      new Date(storeData.expiration) > new Date()
+    ) {
+      login(storeData.userId, storeData.token, new Date(storeData.expiration));
+    }
+  }, [login]);
 
   let routes;
 
